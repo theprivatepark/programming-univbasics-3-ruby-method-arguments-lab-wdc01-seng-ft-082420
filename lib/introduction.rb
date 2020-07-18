68775b166ec55fdd72eadad15e78885d875cab1b
Anney Park <anneypark@gmail.com>
2020-07-18 21:21:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,15 @@
 def introduction(name)
-  "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name,language)
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language_optional(name, language="Ruby")
-  "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
-introduction()
 
 
 
